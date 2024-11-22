39bdb19d17fa2dcfd960cc732579cecff7fcd2e9
Tiberiu Chirila <t.chirila@zippeuk.co.uk>
2024-11-22 15:40:00 +0000

    firebase with real-time database

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBzRvofcCImJJZ_P6BLsF6IGiZcXq7b2cc",
+  authDomain: "todo-4cc4f.firebaseapp.com",
+  projectId: "todo-4cc4f",
+  storageBucket: "todo-4cc4f.firebasestorage.app",
+  messagingSenderId: "813728957854",
+  appId: "1:813728957854:web:3296f97bf1bb356f45828f",
+};
+
+const app = initializeApp(firebaseConfig);
